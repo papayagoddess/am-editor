b5acd77612ddb419a2a4a573d78fdc05fc55e427
papayagoddess <irbth.cl@gmail.com>
2026-03-26 20:17:17 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,36 @@
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+const CACHE_NAME = 'ascribe-v1';
+const ASSETS = [
+  '/ascribe/',
+  '/ascribe/index.html',
+  '/ascribe/manifest.json',
+  '/ascribe/icon.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate & Clean up old caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetching content
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
